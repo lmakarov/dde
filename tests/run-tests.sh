325ad7f119e5c28381d7626fecb1ad2f6567200c
Vasyl Grygoryev <vasyl.grygoryev@ffwagency.com>
2016-07-03 18:06:50 +0300

    Add script for running all tests

---
+++ tests/run-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Switch to drude-d7-testing folder.
+cd ../../drude-d7-testing
+
+# Run tests. Order is important.
+echo "Test command: start"
+bats ../drude/tests/start.bats
+echo "Test command: init"
+bats ../drude/tests/init.bats
+echo "Test command: stop"
+bats ../drude/tests/stop.bats
+echo "Test command: reset"
+bats ../drude/tests/reset.bats
+echo "Test command: restart"
+bats ../drude/tests/restart.bats
+echo "Test command: drush"
+bats ../drude/tests/drush.bats
+echo "Test command: exec"
+bats ../drude/tests/exec.bats
+echo "Test command: ssh-add"
+bats ../drude/tests/ssh-add.bats
+echo "Test command: status"
+bats ../drude/tests/status.bats
+echo "Test command: update"
+bats ../drude/tests/update.bats
